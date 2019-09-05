09965159203ea48eb9256113008402507f6d5004
Cristiam Sosa <csosaur@gmail.com>
2019-09-05 18:00:35 +0200

    Extract doctor interactor

--- src/commands/doctor.ts
+++ src/commands/doctor.ts
@@ -2,7 +2,7 @@ import {flags} from '@oclif/command'
 import {getCustomRepository} from 'typeorm'
 
 import Command from '../base'
-import {Setting} from '../entities/setting'
+import {DoctorInteractor} from '../interactors/doctor.interactor'
 import {getFileService} from '../msa-js'
 import {SettingRepository} from '../repository/setting.repository'
 
@@ -15,52 +15,10 @@ export class Doctor extends Command {
 
   private readonly settingRepository = getCustomRepository(SettingRepository)
   private readonly fileService = getFileService(this.config.home)
+  private readonly interactor = new DoctorInteractor(this.outputService, this.fileService, this.settingRepository)
 
   async run() {
     const parse = this.parse(Doctor)
-
-    const settings = await this.settingRepository.find()
-    const files = this.fileService.getAllSettingFiles()
-
-    const selectedSettings = settings.filter(s => s.isSelected())
-    const hasOnlyOneSettingSelected = selectedSettings.length <= 1
-    if (!hasOnlyOneSettingSelected) {
-      this.outputService.warning('There is more than one settings selected')
-    }
-
-    const allSettingsHasFile = settings.every(setting => this.hasFile(setting, files))
-    if (!allSettingsHasFile) {
-      this.outputService.warning('There are settings without a file')
-    }
-
-    if (parse.flags.fix) {
-      const newSettings = files.map(this.toSetting)
-      newSettings.push(Setting.DEFAULT)
-
-      this.fileService.deleteSelectedSetting()
-      await this.settingRepository.clear()
-      await this.settingRepository.save(newSettings)
-
-      this.outputService.info('Data base has restored')
-    }
-
-    if (hasOnlyOneSettingSelected && allSettingsHasFile && !parse.flags.fix) {
-      this.outputService.success('Everything is fine!!!')
-    }
-  }
-
-  // noinspection JSMethodCanBeStatic
-  private hasFile(setting: Setting, files: string[]) {
-    if (setting.isDefault()) {
-      return true
-    }
-
-    return files.includes(setting.file)
-  }
-
-  // noinspection JSMethodCanBeStatic
-  private toSetting(file: string) {
-    const fileParts = file.split('.')
-    return new Setting(fileParts[0])
+    await this.interactor.execute(parse.flags.fix)
   }
 }
---
+++ src/interactors/doctor.interactor.ts
@@ -0,0 +1,63 @@
+import {Setting} from '../entities/setting'
+import {SettingRepository} from '../repository/setting.repository'
+import {FileService} from '../services/file.service'
+import {OutputService} from '../services/output.service'
+
+import {Interactor} from './interactor'
+
+export class DoctorInteractor extends Interactor<boolean, void> {
+  constructor(
+    private readonly outputService: OutputService,
+    private readonly fileService: FileService,
+    private readonly settingRepository: SettingRepository
+  ) {
+    super()
+  }
+
+  protected async _execute(fix: boolean): Promise<void> {
+    const settings = await this.settingRepository.find()
+    const files = this.fileService.getAllSettingFiles()
+
+    const selectedSettings = settings.filter(s => s.isSelected())
+    const hasOnlyOneSettingSelected = selectedSettings.length <= 1
+    if (!hasOnlyOneSettingSelected) {
+      this.outputService.warning('There is more than one settings selected')
+    }
+
+    const allSettingsHasFile = settings.every(setting => this.hasFile(setting, files))
+    if (!allSettingsHasFile) {
+      this.outputService.warning('There are settings without a file')
+    }
+
+    if (fix) {
+      const newSettings = files.map(this.toSetting)
+      newSettings.push(Setting.DEFAULT)
+
+      this.fileService.deleteSelectedSetting()
+      await this.settingRepository.clear()
+      await this.settingRepository.save(newSettings)
+
+      this.outputService.info('Data base has restored')
+    }
+
+    if (hasOnlyOneSettingSelected && allSettingsHasFile && !fix) {
+      this.outputService.success('Everything is fine!!!')
+    }
+  }
+
+  // noinspection JSMethodCanBeStatic
+  private hasFile(setting: Setting, files: string[]) {
+    if (setting.isDefault()) {
+      return true
+    }
+
+    return files.includes(setting.file)
+  }
+
+  // noinspection JSMethodCanBeStatic
+  private toSetting(file: string) {
+    const fileParts = file.split('.')
+    return new Setting(fileParts[0])
+  }
+
+}
