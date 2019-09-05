8421d0131ed06595768f5e8b078af56eb978b53c
Cristiam Sosa <csosaur@gmail.com>
2019-09-05 19:00:44 +0200

    Extract use interactor

--- src/commands/use.ts
+++ src/commands/use.ts
@@ -1,8 +1,8 @@
 import {flags} from '@oclif/command'
-import chalk from 'chalk'
 import {getCustomRepository} from 'typeorm'
 
 import Command from '../base'
+import {UseSettingInteractor} from '../interactors/use-setting.interactor'
 import {getFileService} from '../msa-js'
 import {SettingRepository} from '../repository/setting.repository'
 
@@ -20,28 +20,10 @@ export class Use extends Command {
 
   private readonly settingRepository = getCustomRepository(SettingRepository)
   private readonly fileService = getFileService(this.config.home)
+  private readonly interactor = new UseSettingInteractor(this.outputService, this.fileService, this.settingRepository)
 
   async run() {
     const parse = this.parse(Use)
-
-    if (!parse.args.setting) {
-      const settings = await this.settingRepository.find()
-      parse.args.setting = await this.outputService.selectSetting('Choose the setting to be used', settings)
-    }
-
-    const newSetting = await this.settingRepository.findOneByName(parse.args.setting)
-    const oldSetting = await this.settingRepository.findSelected()
-
-    if (oldSetting) {
-      this.fileService.deactivateSetting(oldSetting)
-      oldSetting.unselect()
-      await this.settingRepository.save(oldSetting)
-    }
-
-    this.fileService.activateSetting(newSetting)
-    newSetting.select()
-    await this.settingRepository.save(newSetting)
-
-    this.outputService.success(`You activate ${chalk.bold.yellow(parse.args.setting)} setting`)
+    await this.interactor.execute(parse.args.setting)
   }
 }
---
+++ src/interactors/use-setting.interactor.ts
@@ -0,0 +1,39 @@
+import chalk from 'chalk'
+
+import {SettingRepository} from '../repository/setting.repository'
+import {FileService} from '../services/file.service'
+import {OutputService} from '../services/output.service'
+
+import {Interactor} from './interactor'
+
+export class UseSettingInteractor extends Interactor<string, void> {
+  constructor(
+    private readonly outputService: OutputService,
+    private readonly fileService: FileService,
+    private readonly settingRepository: SettingRepository
+  ) {
+    super()
+  }
+
+  protected async _execute(setting: string): Promise<void> {
+    if (!setting) {
+      const settings = await this.settingRepository.find()
+      setting = await this.outputService.selectSetting('Choose the setting to be used', settings)
+    }
+
+    const newSetting = await this.settingRepository.findOneByName(setting)
+    const oldSetting = await this.settingRepository.findSelected()
+
+    if (oldSetting) {
+      this.fileService.deactivateSetting(oldSetting)
+      oldSetting.unselect()
+      await this.settingRepository.save(oldSetting)
+    }
+
+    this.fileService.activateSetting(newSetting)
+    newSetting.select()
+    await this.settingRepository.save(newSetting)
+
+    this.outputService.success(`You activate ${chalk.bold.yellow(setting)} setting`)
+  }
+}
